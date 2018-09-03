1d8550ae95dca603a1c4b0c895eb1b72f92907ee
searleser97 <serchgabriel97@gmail.com>
2018-09-03 09:06:12 -0500

    updated: 03/09/2018 09:06:12 AM

--- main.js
+++ main.js
@@ -3,81 +3,85 @@
 ===================================================================*/
 
 // Autómata 1
-// var a1 = new Automata();
-// a1.basico('+');
-// var a2 = new Automata();
-// a2.basico('-');
-// a1.unir(a2);
-// a1.cerraduraInterrogacion();
-// var a3 = new Automata();
-// a3.basico('D');
-// a3.cerraduraPositiva();
-// a1.concatenar(a3);
+var a1 = new Automata();
+a1.basico('+');
+var a2 = new Automata();
+a2.basico('-');
+a1.unir(a2);
+a1.cerraduraInterrogacion();
+var a3 = new Automata();
+a3.basico('D');
+a3.cerraduraPositiva();
+a1.concatenar(a3);
 // createFSMDiagram(a1);
 
 
 
 // Autómata 2
-// var a4 = new Automata();
-// a4.basico('+');
-// var a5 = new Automata();
-// a5.basico('-');
-// a4.unir(a5);
-// a4.cerraduraInterrogacion();
-// var a6 = new Automata();
-// a6.basico('D');
-// a6.cerraduraPositiva();
-// a4.concatenar(a6);
-// var a7 = new Automata();
-// a7.basico('.');
-// a4.concatenar(a7);
-// var a7 = new Automata();
-// a7.basico('D');
-// a7.cerraduraPositiva();
-// a4.concatenar(a7);
-// var a8 = new Automata();
-// a8.basico('E');
-// var a9 = new Automata();
-// a9.basico('e');
-// a8.unir(a9);
-// var a10 = new Automata();
-// a10.basico('+');
-// var a11 = new Automata();
-// a11.basico('-');
-// a10.unir(a11);
-// a10.cerraduraInterrogacion();
-// a8.concatenar(a10);
-// var a12 = new Automata();
-// a12.basico('D');
-// a12.cerraduraPositiva();
-// a8.concatenar(a12);
-// a8.cerraduraInterrogacion();
-// a4.concatenar(a8);
+var a4 = new Automata();
+a4.basico('+');
+var a5 = new Automata();
+a5.basico('-');
+a4.unir(a5);
+a4.cerraduraInterrogacion();
+var a6 = new Automata();
+a6.basico('D');
+a6.cerraduraPositiva();
+a4.concatenar(a6);
+var a7 = new Automata();
+a7.basico('.');
+a4.concatenar(a7);
+var a7 = new Automata();
+a7.basico('D');
+a7.cerraduraPositiva();
+a4.concatenar(a7);
+var a8 = new Automata();
+a8.basico('E');
+var a9 = new Automata();
+a9.basico('e');
+a8.unir(a9);
+var a10 = new Automata();
+a10.basico('+');
+var a11 = new Automata();
+a11.basico('-');
+a10.unir(a11);
+a10.cerraduraInterrogacion();
+a8.concatenar(a10);
+var a12 = new Automata();
+a12.basico('D');
+a12.cerraduraPositiva();
+a8.concatenar(a12);
+a8.cerraduraInterrogacion();
+a4.concatenar(a8);
 // createFSMDiagram(a4);
 
 
 
 // Autómata 3
-// var a13 = new Automata();
-// a13.basico('L');
-// var a14 = new Automata();
-// a14.basico('L');
-// var a15 = new Automata();
-// a15.basico('D');
-// a14.unir(a15);
-// a14.cerraduraPositiva();
-// a13.concatenar(a14);
+var a13 = new Automata();
+a13.basico('L');
+var a14 = new Automata();
+a14.basico('L');
+var a15 = new Automata();
+a15.basico('D');
+a14.unir(a15);
+a14.cerraduraPositiva();
+a13.concatenar(a14);
 // createFSMDiagram(a13);
 
 
 // Autómata 4
-// var a16 = new Automata();
-// a16.basico('S');
-// var a17 = new Automata();
-// a17.basico('T');
-// a16.unir(a17);
-// a16.cerraduraPositiva();
-// createFSMDiagram(a16);
+var a16 = new Automata();
+a16.basico('S');
+var a17 = new Automata();
+a17.basico('T');
+a16.unir(a17);
+a16.cerraduraPositiva();
+
+a1.unir(a4);
+a1.unir(a13);
+a1.unir(a16);
+createFSMDiagram(a1);
 
 var automatas = [];
 
