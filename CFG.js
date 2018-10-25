c1c8578d0c8fb9621e1138e391242a649bb271a4
DanielDumile <daniel_gn98@hotmail.com>
2018-10-25 08:51:08 -0500

    Update CFG.js

--- CFG.js
+++ CFG.js
@@ -1,5 +1,4 @@
 //Production: Arreglo de strings
-
 function mapeoRegla(LS, RS){//LS:string RS:Production(Array<string>)
 	this.LS = LS;
 	this.RS = RS;
@@ -14,7 +13,13 @@ function CFG(){
 	_this.rules = new Map();//Map<String,Array<production>>
 	_this.firsts = new Map();//Mapa de conjuntos tal que Map<String,Set<String>>
 	_this.appRS= new Map();//Right Side Appearances
-	_this.follows= new Map();
+	_this.follows= new Map();//Mapa de conjuntos tal que Map<String,Set<String>>
+	_this.tablaLL = new Map()//Sera un mapa de mapas.
+	//En las columnas van los terminales y el $. En las filas van todos (No terminales y terminales)
+	//Adentro de la tabla habra una produccion.
+	//Primer mapa son filas.
+	//Segundo mapa son columnas.
+	//El valor del segundo mapa sera una produccion
 
 	_this.setSNT = function(s){
 		_this.startingNonTerminal = s;
@@ -61,6 +66,9 @@ function CFG(){
 			_this.firsts.set(c,_this.getFirst(c));
 		for(var c of _this.nonTerminals)
 			_this.firsts.set(c,_this.getFirst(c));
+		var auxi = new Set();
+		auxi.add("ep");
+		_this.firsts.set("ep",auxi);
 	}
 	//Set Right Side Appearances
 	_this.setRSApp = function(s){//String s
@@ -116,10 +124,61 @@ function CFG(){
 
 	_this.setFollows = function(){
 		//Gotta set RSApp before getting the follows
-		for(var c of nonTerminals)
+		for(var c of _this.nonTerminals)
 			_this.setRSApp(c);
 		//Setting follows for every non terminal
-		for(var c of nonTerminals)
+		for(var c of _this.nonTerminals)
 			_this.follows.set(c,_this.getFollow(c));
+	}
+	_this.setPreValues= function(){
+		var aux1 = new Map();
+		aux1.set("$",["aceptar"]);
+		_this.tablaLL.set("$",aux1);
+
+		for(var c of terminals){
+			var aux2 = new Map();
+			aux2.set(c,["pop"]);
+			_this.tablaLL.set(c,aux2);
+		}
+	}
+
+	_this.fillTable = function(){
+		_this.setPreValues();
+		for(var [leftSide,rightSide] of _this.rules){
+			var auxMap = new Map();
+			for(var p of rightSide){//P es una prod
+				console.log("Sacamos a la produccion");
+				console.log(p);
+				console.log("Hacemos el first de "+ p[0]);
+
+				var firstF= _this.firsts.get(p[0]);
+				console.log(firstF);
+
+				if(firstF.has("ep")){
+					console.log("Hay un epsilom malvado, se calcula follow");
+					firstF = _this.follows.get(leftSide);
+					console.log("Lo cambiamos y ahora es ");
+					console.log(firstF);
+				}
+				
+				for(var e of firstF){//Por cada elemento del first
+					//var auxMap = new Map();
+					//A cada elemento lo mapeamos con la produccion
+					auxMap.set(e,p);
+					console.log("El map chico nos queda ");
+					console.log(e+ "-> ");
+					console.log(auxMap.get(e));
+					//A la tabla en el valor de la izquierda lo mapeamos con el auxMap	
+				}	
+			}
+			_this.tablaLL.set(leftSide,auxMap);
+			console.log("El map grande nos queda ");
+			console.log(leftSide+"->");
+			console.log(_this.tablaLL.get(leftSide));
+		}
+	}
+
+	_this.tableLL= function(){
+		_this.fillTable();
 	}
 }
